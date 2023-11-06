fd34f5805b28da25db8c7e66984a4d7a8b7ee3fc
MayVarela16 <loly_varela@hotmail.com>
2023-11-07 00:38:31 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
  // Aquí tu código
+ 
+ function addElements() {
+    const nuevoElemento= prompt("Escriba un elemento:");
+    const lista= document.getElementById("lista");
+    const elementoLista= document.createElement("li");
+
+    elementoLista.textContent= nuevoElemento;
+    lista.appendChild(elementoLista);
+}
+
+const botonAgregar= document.getElementById("Agregar");
+ botonAgregar.addEventListener("click", agregarElemento);
+
+ console.log(addElements());
